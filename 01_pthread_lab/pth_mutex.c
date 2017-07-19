8874772f2ed54c235a325f89bce771318e609dce
Jeong-Gun Lee <jeonggun.lee@gmail.com>
2017-07-19 12:37:22 +0900

    Create pth_mutex.c

---
+++ 01_pthread_lab/pth_mutex.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <pthread.h>
+#define MAX_SIZE  5
+
+pthread_mutex_t bufLock;
+int count;
+
+void producer(char* buf) {
+   for(;;) {
+        while(count == MAX_SIZE);
+        pthread_mutex_lock(&bufLock);
+        buf[count] = 'a';
+        count++;
+        printf("Consume Char\n");
+        pthread_mutex_unlock(&bufLock);
+   }
+}
+void consumer(char* buf) {
+   for(;;) {
+        while(count == 0);
+        pthread_mutex_lock(&bufLock);
+        //useChar(buf[count-1]);
+        printf("Use Char \n");
+        count--;
+        pthread_mutex_unlock(&bufLock);
+   }
+}
+
+int main() {
+   char buffer[MAX_SIZE];
+   pthread_t p;
+   count = 0;
+   pthread_mutex_init(&bufLock, NULL);
+   pthread_create(&p, NULL, (void*)producer, &buffer);
+   consumer(buffer);
+   return 0;
+}
