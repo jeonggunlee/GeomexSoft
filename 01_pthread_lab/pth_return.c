dbf77dbab827bab1ef8790b5b6dd2fb5005ec5fe
Jeong-Gun Lee <jeonggun.lee@gmail.com>
2017-07-19 12:37:48 +0900

    Create pth_return.c

---
+++ 01_pthread_lab/pth_return.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void *thread(void *vargp) {
+        int *value = (int *)malloc(sizeof(int));
+        *value = 42;
+        pthread_exit(value);
+}
+
+int main() {
+        int i;
+        pthread_t tid;
+        void *vptr_return;
+
+        pthread_create(&tid, NULL, thread, NULL);
+        pthread_join(tid, &vptr_return);
+
+        i = *((int *)vptr_return);
+        free(vptr_return);
+        printf("%d\n",i);
+}
